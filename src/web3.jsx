cee4743aca83e2cd0e10e6805faff25bf9a7a50d
alex <101093214+AexLiuMing@users.noreply.github.com>
2023-03-09 19:11:25 +0800

    first commit

--- src/web3.jsx
+++ src/web3.jsx
@@ -2,5 +2,5 @@ import { configs } from './config'
 const { NETWORK_ID, INFURA_APP_ID } = configs
 const ethers = require('ethers')
 window.ethers = ethers
-const eth = new ethers.providers.InfuraProvider(NETWORK_ID, INFURA_APP_ID)
+const eth = new ethers.providers.JsonRpcProvider("http://192.168.20.107:8545",NETWORK_ID)
 export default eth
