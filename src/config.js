3f35379a81481b3ba423a1830f0a1c51102008cc
alex <101093214+AexLiuMing@users.noreply.github.com>
2023-03-16 14:54:50 +0800

    fix: arrange config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,12 @@
 export const configs = {
-  NETWORK_ID: 2137,
   // INFURA_APP_ID: "79a94efd744448b980ba2daab9575b31",
   // ETHERSCAN_URL: "https://etherscan.io",
-
   // todd： 使用的是dss-chain-log
   // https://github.com/makerdao/dss-chain-log
-  CHAINLOG_URL: "http://192.168.20.107:8080",
   // ETHERSCAN_ETHSUPPLY_URL:
   //   "https://api.etherscan.io/api?action=ethsupply&module=stats&apikey=N5TICDBVG4MHDS7CGPJ9MHXRYC1Y84963N",
+
+  NETWORK_ID: 2137,
+  RPC_URL: "http://192.168.20.107:8545",
+  CHAINLOG_URL: "http://192.168.20.107:8080",
 };
--- src/web3.jsx
+++ src/web3.jsx
@@ -1,7 +1,7 @@
 import { configs } from './config'
-const { NETWORK_ID, INFURA_APP_ID } = configs
+const { NETWORK_ID, RPC_URL } = configs
 const ethers = require('ethers')
 window.ethers = ethers
 //todd 修改jsonrpc为localhost:8545 + networkid
-const eth = new ethers.providers.JsonRpcProvider("http://192.168.20.107:8545",NETWORK_ID)
+const eth = new ethers.providers.JsonRpcProvider(RPC_URL,NETWORK_ID)
 export default eth
